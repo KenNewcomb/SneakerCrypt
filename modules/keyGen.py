2a9f111c9216ac71a8b8698c6aafdaeef8a33c51
Ken Newcomb <knewco02@villanova.edu>
2014-07-30 16:35:05 -0400

    fixed

--- modules/keyGen.py
+++ modules/keyGen.py
@@ -1,17 +1,12 @@
-from random import SystemRandom
+import random
 import time
 
 def get(numbits):
-	seed = time.time()
-	r1 = SystemRandom(seed)
-	return r1.randint(0, 2**numbits -1)
+	return random.randint(0, 2**numbits -1)
 
 def getPad(numKiloBytes):
-	seed = time.time()
-	r1 = SystemRandom(seed)
 	key = 0
 	for i in range(1,numKiloBytes):
-		part = r1.randint(0,2**(8000) -1)
+		part = random.randint(0,2**(8000) -1)
 		key = int(str(key) + str(part))
 		print("Generated " + str(i) + " kilobytes")
-	print(key)

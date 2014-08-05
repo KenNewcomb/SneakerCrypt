df366c492e2a3a1f06df3f18a1590e574e04e49e
Ken Newcomb <knewco02@villanova.edu>
2014-08-05 16:26:57 -0400

    Code consolidation

--- sneakercrypt.py
+++ sneakercrypt.py
@@ -7,13 +7,19 @@ from autobahn.twisted.websocket import WebSocketServerFactory
 
 if __name__ == '__main__':
 
-	log.startLogging(sys.stdout)
-	while True:
-		
-		messageToSend = input("Enter a message to send")
+	log.startLogging(sys.stdout) # Start logging to console
+	mode = sys.argv[0] # Get server mode from cmdline argument.
 
+	if mode == "server": # Start server
 		factory = WebSocketServerFactory()
 		factory.protocol = serverProtocol
-
 		reactor.listenTCP(9000, factory)
 		reactor.run()
+	elif mode == "client": # Start client
+		factory = WebSocketClientFactory()
+		factory.protocol = MyClientProtocol
+		reactor.connectTCP("127.0.0.1", 9000, factory)
+		reactor.run()
+	
+	messageToSend = input("Enter a message to send:")
+

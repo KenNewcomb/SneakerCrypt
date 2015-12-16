8e6b798847d6ceefd6d0a9b5788439a1c15ef441
Ken Newcomb <KenNewcombNJ@gmail.com>
2015-12-16 13:20:55 -0500

    Added logo.

--- sneakercrypt.py
+++ sneakercrypt.py
@@ -12,12 +12,21 @@ def usage():
 	print("\t4.) decrypt  - Decrypt message")
 	exit()
 
+def logo():
+	print("""                         _                                      
+   ()                   | |                                     
+   /\  _  _    _   __,  | |   _   ,_    __   ,_           _ _|_ 
+  /  \/ |/ |  |/  /  |  |/_) |/  /  |  /    /  |  |   | |/ \_|  
+ /(__/  |  |_/|__/\_/|_/| \_/|__/   |_/\___/   |_/ \_/|/|__/ |_/
+                                                     /|/|       
+                                                     \|\|    """)
+logo()
 # Sanitize input
 if len(sys.argv) != 2:
 	usage()
 
+# Get mode, take appropriate action
 mode = sys.argv[1]
-
 if mode == 'pads':
 	pads.printPads()
 elif mode == 'generate':
